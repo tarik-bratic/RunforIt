01d469aca5b8faedbb9f49c87bb53f382d59c27e
Tariq <tarik.bratic@gmx.com>
2024-12-11 14:05:30 +0100

    Initial Commit

---
+++ Run for It/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Run for It
+//
+//  Created by Tarik Bratic on 2024-12-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Run for It/Run_for_ItApp.swift
@@ -0,0 +1,17 @@
+//
+//  Run_for_ItApp.swift
+//  Run for It
+//
+//  Created by Tarik Bratic on 2024-12-11.
+//
+
+import SwiftUI
+
+@main
+struct Run_for_ItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
